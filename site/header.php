54e82c8eedd74de2900fabdfa4bd49eae37cfe7a
Darilah <116173161+Darilah@users.noreply.github.com>
2022-11-20 17:26:03 +0100

    Add files via upload

---
+++ site/header.php
@@ -0,0 +1,33 @@
+<header id="header" class="col-md-auto">
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000;">
+    <div class="container-fluid">
+      <img src="image.jpg" alt="" width="100" height="55">
+        <ul class="navbar">
+          <li class="nav-item mr-auto">
+            <a class="nav-link-active" aria-current="page" href="#">ACCUEIL</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ARTISTES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GENRES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+          </li>
+        </ul>
+        <ul class="nav col-md-1 col-md-offset-10">
+          <div class="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="./images/oe.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small dropdown-menu-dark" style="background-color: #000000; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(0px, 34px);" data-popper-placement="bottom-start">
+              <li><a class="dropdown-item" href="#">Mes favoris</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Se déconnecter</a></li>
+            </ul>
+          </div>
+        </ul>
+    </div>
+  </nav>
+</header>
---
+++ site/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue</title>
+    
+
+    <!--Bootstrap css-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    
+    <!--fichier css-->
+    <link rel="stylesheet" href="./style.css">
+
+    <!--Bootstrap java -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    
+</head>
+<body class="d-flex flex-column min-vh-100 w-auto">
+    <div class="container-fluid">
+
+    <?php include_once('header.php'); ?>
+        <h1>Bonjour</h1>
+
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
+
