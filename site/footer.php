7a2bce3d7db7678f77f8d2775915a5d2003f0189
Tiwi237 <82931306+Tiwi237@users.noreply.github.com>
2022-12-02 10:25:55 +0100

    Add files via upload

---
+++ site/footer.php
@@ -0,0 +1,41 @@
+<footer id="footer" class="col-md-auto">
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000;">
+    <div class="container-fluid">
+      <img src="image.jpg" alt="" width="100" height="55">
+        <ul class="navbar list-unstyled col-md-5 col-md-offset-1">
+          <li class="nav-item mr-auto">
+            <a class="nav-link-active" aria-current="page" href="index.php">ACCUEIL</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ARTISTES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GENRES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">CONTACT</a>
+          </li>
+        </ul>
+        <!-- Dropdown -->
+          <ul class="nav col-md-1 col-md-offset-10">
+            <div class="dropup">
+              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="./images/oe.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
+              </a>
+              <ul class="dropdown-menu text-small dropdown-menu-dark" style="background-color: #000000; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(0px, 34px);" data-popper-placement="bottom-start">
+                <li><a class="dropdown-item" href="#">Mes favoris</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="logout.php">Se déconnecter</a></li>
+              </ul>
+            </div>
+        </ul>
+        <!-- Dropdown -->
+    </div>
+  </nav>
+  <!-- Copyright -->
+  <div class="text-center p-2" style="background-color: #000000;">
+    © 2020 Copyright:
+    <a class="text-white" href="https://bootstrap.com/">Bootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
