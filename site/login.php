db495e5eac4dec7c5823773fec56708db4296ef7
Tiwi237 <82931306+Tiwi237@users.noreply.github.com>
2022-12-04 13:40:55 +0100

    Update login.php

--- site/login.php
+++ site/login.php
@@ -39,12 +39,12 @@ if (isset($_POST['email']) &&  isset($_POST['password'])) {
     <?php endif; ?>
     <div class="mb-3 col-5">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
+        <input type="email" class="form-control-center" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
         <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
     </div>
     <div class="mb-3 col-5">
         <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
+        <input type="password" class="form-control-center" id="password" name="password">
     </div>
     <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
