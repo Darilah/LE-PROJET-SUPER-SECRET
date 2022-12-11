2f33fc31e8b05fc85c6f730baeaa06fecec98a9e
Tiwi237 <82931306+Tiwi237@users.noreply.github.com>
2022-12-11 13:32:36 +0100

    Add files via upload

---
+++ genre.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 11, 2022 at 12:31 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `idGenre` int(11) NOT NULL COMMENT 'Identifiant unique du genre',
+  `genre` varchar(255) COLLATE utf8_bin NOT NULL COMMENT 'Genre musical de l''artiste'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`idGenre`, `genre`) VALUES
+(1, 'Pop'),
+(2, 'Rock'),
+(3, 'Rap'),
+(4, 'Electro'),
+(5, 'Indie'),
+(6, 'Punk');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`idGenre`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `idGenre` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifiant unique du genre', AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
